e343e0189eee4d76db73ed4a3cabb4d3e6aaf93e
Edgar BAGHDASARYAN <edgar.baghdasaryan@ens.uvsq.fr>
2024-03-15 12:57:26 +0100

    dans la partie logique : ajout de la fonction pour ajouter un jeton dans un ensemble

--- logic.py
+++ logic.py
@@ -1,9 +1,12 @@
 import numpy as np
 
 class Logic :
-    def __init__(self, size):
+    def __init__(self, size, current_player):
         self.size = size
         self.board = np.zeros((size, size))
+        self.sets = {"red" : [], "blue" : []}
+        self.boarder = {"red" : {0 : "top", size-1 : "bottom"}, "blue" : {0 : "left", size-1 : "right"}}
+        self.current_player = current_player
 
     def get_neighbours(self, x, y):
         for offset_x, offset_y in [(-1, 0), (-1, 1), (0, 1), (1, 0), (1, -1), (0, -1)]:
@@ -12,3 +15,19 @@ class Logic :
 
     def place_token(self, new_color, x, y):
         self.board[x, y] = new_color
+
+    def add_token(self, x, y):
+        neighbor_sets = []
+        sets = self.sets[self.current_player]
+        for neighbor in self.get_neighbors(x,y):
+            for set in sets:
+                if neighbor in set:
+                    neighbor_sets.append(set)
+        new_set = {(x, y)}
+        while len(neighbor_sets) > 0:
+            neighbor_sets[-2] = neighbor_sets[-2].union(neighbor_sets[-1])
+            del(neighbor_sets[-1])
+        new_set = new_set.union(neighbor_sets[0])
+        boarder = self.boarder[self.current_player].get(y)
+        if boarder != None :
+            new_set.add(boarder)
