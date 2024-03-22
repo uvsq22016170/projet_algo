3a5e2b3bfb246c44a483708fe19bff9299e79429
uvsqNathanAmoussou <nathan.amoussou@ens.uvsq.fr>
2024-03-22 11:10:14 +0100

    Modification menu

--- user_interface.py
+++ user_interface.py
@@ -1,70 +1,54 @@
+# Import
 import pygame
 import pygame_menu
 
+# Initialisation de Pygame
 pygame.init()
-surface = pygame.display.set_mode((720, 720))
 
-class MainMenu:
-    def __init__(self, surface):
-        self.surface = surface
-        self.menu = pygame_menu.Menu('Menu principal', 720, 720)
-        self.menu.add.button('Créer une nouvelle partie', self.create_new_game)
-        self.menu.add.button('Charger une sauvegarde', self.load_save)
-        self.menu.add.button('Quitter', pygame_menu.events.EXIT)
+largeur = 900
+longueur = 700
+surface = pygame.display.set_mode((largeur, longueur))
 
-    def create_new_game(self):
-        new_game_menu = NewGameMenu(self.surface)
-        new_game_menu.run()
+# Menu principal
+def main_menu():
+    menu_principal = pygame_menu.Menu('Menu Principal', largeur, longueur)
+    menu_principal.add.button('Créer une nouvelle partie', create_new_game)
+    menu_principal.add.button('Charger une sauvegarde', load_saved_game)
+    menu_principal.add.button('Quitter', pygame_menu.events.EXIT)
+    menu_principal.mainloop(surface)
 
-    def load_save(self):
-        load_save_menu = LoadSave(self.surface)
-        load_save_menu.run()
+def create_new_game():
+    new_game_menu = pygame_menu.Menu('Nouvelle Partie', largeur, longueur)
+    new_game_menu.add.button('Joueur contre joueur', PVP)
+    new_game_menu.add.button('Joueur contre IA')
+    new_game_menu.add.button('Retour', main_menu)
+    new_game_menu.mainloop(surface)
 
-    def run(self):
-        self.menu.mainloop(self.surface)
+def load_saved_game():
+    saved_game_menu = pygame_menu.Menu('Charger une sauvegarde', largeur, longueur)
+    saved_game_menu.add.button('Charger la sauvegarde 1')
+    saved_game_menu.add.button('Charger la sauvegarde 2')
+    saved_game_menu.add.button('Charger la sauvegarde 3')
+    saved_game_menu.add.button('Retour', main_menu)
+    saved_game_menu.mainloop(surface)
 
-class NewGameMenu:
-    def __init__(self, surface):
-        self.surface = surface
-        self.menu = pygame_menu.Menu('Nouvelle partie', 720, 720)
-        self.menu.add.button('Joueur contre joueur', self.PVP)
-        self.menu.add.button('Joueur contre IA', self.PVE)
-        self.menu.add.button('Retour', pygame_menu.events.BACK)
+# Créer une nouvelle partie
+def PVP():
+    PVP_menu = pygame_menu.Menu('Joueur contre joueur', largeur, longueur)
+    PVP_menu.add.button('Taille du quadrillage', grid_size)
+    PVP_menu.add.button('Joueur qui commence', player_starts)
+    PVP_menu.add.button('Lancer la partie', start_game)
+    PVP_menu.add.button('Retour', create_new_game)
+    PVP_menu.mainloop(surface)
 
-    def PVP(self):
-        pass
+def grid_size():
+    pass
 
-    def PVE(self):
-        pass
+def player_starts():
+    pass
 
-    def run(self):
-        self.menu.mainloop(self.surface)
-
-    def back(self):
-        new_game_menu = NewGameMenu(self.surface)
-        new_game_menu.run()
-
-class LoadSave:
-    def __init__(self, surface):
-        self.surface = surface
-        self.menu = pygame_menu.Menu('Charger une sauvegarde', 720, 720)
-        self.menu.add.button('Charger la sauvegarde 1', self.save1)
-        self.menu.add.button('Charger la sauvegarde 2', self.save2)
-        self.menu.add.button('Charger la sauvegarde 3', self.save3)
-        self.menu.add.button('Retour', pygame_menu.events.BACK)
-
-    def save1(self):
-        pass
-
-    def save2(self):
-        pass
-
-    def save3(self):
-        pass
-
-    def run(self):
-        self.menu.mainloop(self.surface)
+def start_game():
+    pass
 
 if __name__ == "__main__":
-    main_menu = MainMenu(surface)
-    main_menu.run()
+    main_menu()
